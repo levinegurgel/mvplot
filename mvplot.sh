b4211aa7c65cc5517bbbd3b3c4673d6695c17d80
Levine Gurgel <levinegurgelf@gmail.com>
2021-08-11 12:17:16 -0300

    Retirando ; no final das declaraçãoes de variáveis

--- mvplot.sh
+++ mvplot.sh
@@ -6,19 +6,17 @@ This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.
 Written by Levine <levinegurgelf@gmail.com>"
 
-partition1="$HOME/Desktop/Trabalho/EcoHack/mvplot/partition1/";
-partition2="$HOME/Desktop/Trabalho/EcoHack/mvplot/partition2/";
-partition3="$HOME/Desktop/Trabalho/EcoHack/mvplot/partition3/";
-log="$HOME/Desktop/Trabalho/EcoHack/mvplot/mvplot.log";
+partition1="$HOME/Desktop/Trabalho/EcoHack/mvplot/partition1/"
+partition2="$HOME/Desktop/Trabalho/EcoHack/mvplot/partition2/"
+partition3="$HOME/Desktop/Trabalho/EcoHack/mvplot/partition3/"
+
+log="$HOME/Desktop/Trabalho/EcoHack/mvplot/mvplot.log"
 fileType="plot"
-
 file="$(basename $partition1/*.$fileType)"
-filePartition1=$partition1$file
 
+if [ -f $partition1$file ]; then
 
-if [ -f $filePartition1 ]; then
-
- mv $filePartition1 $partition2 && mv  $partition2$file $partition3
- echo "$(date +%Y%m%d%H%M%S) movendo o arquivo $filePartition1 para a partição $partition2 e movendo da $partition2 para a $partition3" >> $log
+ mv $partition1$file $partition2 && mv  $partition2$file $partition3
+ echo "$(date +%Y%m%d%H%M%S) movendo o arquivo $partition1$file para a partição $partition2 e movendo da $partition2 para a $partition3" >> $log
 
 fi
